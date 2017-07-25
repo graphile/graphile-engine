9873881a5f2846713101d39260170972ab5804cf
Benjie Gillam <benjie@jemjie.com>
2017-07-25 14:55:25 +0100

    Tweak flow export

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 // @flow
-/*::
-export type * from './src';
-*/
 
 // This script detects if you're running on Node v8 or above; if so it runs the
 // code directly, otherwise it falls back to the babel-compiled version
 
-if (process.versions.node.match(/^([89]|[1-9][0-9]+)\./)) {
-  // Modern node, run verbatim
-  module.exports = require("./src");
-} else {
-  // Older node, run compiled code
-  module.exports = require("./lib");
-}
+/*::
+// Trick flow into exporting the types
+export type * from './src';
+*/
+
+const isNode8Plus = process.versions.node.match(/^([89]|[1-9][0-9]+)\./);
+module.exports = isNode8Plus ? require("./src") : require("./lib");
