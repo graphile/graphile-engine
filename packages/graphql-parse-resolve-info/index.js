953da9586ab3e07d50b8190ab95777eb935b66f5
Benjie Gillam <benjie@jemjie.com>
2017-07-25 13:16:24 +0100

    Change flow export trick

--- packages/graphql-parse-resolve-info/index.js
+++ packages/graphql-parse-resolve-info/index.js
@@ -3,6 +3,11 @@
 // This script detects if you're running on Node v8 or above; if so it runs the
 // code directly, otherwise it falls back to the babel-compiled version
 
+/*::
+// Trick flow into exporting the types
+export type * from './node8plus';
+*/
+
 if (process.versions.node.match(/^([89]|[1-9][0-9]+)\./)) {
   // Modern node
   module.exports = require("./node8plus");
