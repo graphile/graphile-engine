488ed2eb0f405e1ed6e2f7f40541140376b26238
Benjie Gillam <benjie@jemjie.com>
2017-08-01 10:38:33 +0100

    Add explanation

--- packages/graphql-build-pg/src/plugins/PgMutationCreatePlugin.js
+++ packages/graphql-build-pg/src/plugins/PgMutationCreatePlugin.js
@@ -192,6 +192,31 @@ export default (function PgMutationCreatePlugin(
                     return pgClient.query(text, values);
                   }
 
+                  /*
+                   * Originally we tried this with a CTE, but:
+                   *
+                   * > The sub-statements in WITH are executed concurrently
+                   * > with each other and with the main query. Therefore, when
+                   * > using data-modifying statements in WITH, the order in
+                   * > which the specified updates actually happen is
+                   * > unpredictable. All the statements are executed with the
+                   * > same snapshot (see Chapter 13), so they cannot "see" one
+                   * > another's effects on the target tables. This alleviates
+                   * > the effects of the unpredictability of the actual order
+                   * > of row updates, and means that RETURNING data is the only
+                   * > way to communicate changes between different WITH
+                   * > sub-statements and the main query.
+                   *
+                   * -- https://www.postgresql.org/docs/9.6/static/queries-with.html
+                   *
+                   * This caused issues with computed columns that themselves
+                   * went off and performed selects - because the data within
+                   * those selects used the old snapshot and thus returned
+                   * stale data. To solve this, we now use temporary tables to
+                   * ensure the mutation and the select execute in different
+                   * statments.
+                   */
+
                   await performQuery(
                     pgClient,
                     sql.query`
