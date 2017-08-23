bd5d69be230821aed87e3b12ede610527797b65e
Benjie Gillam <benjie@jemjie.com>
2017-08-23 10:40:35 +0100

    Clearer solution to warning

--- packages/graphile-build/src/makeNewBuild.js
+++ packages/graphile-build/src/makeNewBuild.js
@@ -187,7 +187,7 @@ export default function makeNewBuild(builder: SchemaBuilder): Build {
         console.warn(
           `No scope was provided to new ${getNameFromType(
             Type
-          )}[name=${spec.name}], please check usage of 'newWithHooks'`
+          )}[name=${spec.name}], it's highly recommended that you add a scope so other hooks can easily reference your object - please check usage of 'newWithHooks'. To mute this message, just pass an empty object.`
         );
       }
       if (!Type) {
