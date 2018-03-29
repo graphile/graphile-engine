0c529cc44ff4ebfe292c83d0e03bfdd62ce13b91
Jem <jem@jemjie.com>
2018-03-29 13:49:49 +0100

    Removed unneeded col2

--- packages/postgraphile-core/__tests__/kitchen-sink-schema.sql
+++ packages/postgraphile-core/__tests__/kitchen-sink-schema.sql
@@ -378,9 +378,10 @@ $$ language plpgsql stable;
 
 -- Begin tests for smart comments
 
+-- Rename table and columns
+
 create table d.original_table (
-  col1 int,
-  col2 int
+  col1 int
 );
 
 comment on table d.original_table is E'@name renamed_table';
