c351a143737c9912e2efe0a5978c40eefb74195b
Jem <jem@jemjie.com>
2018-04-04 14:21:21 +0100

    Tests for single omit actions

--- packages/postgraphile-core/__tests__/integration/schema/omit-rename.omitstuff.test.js
+++ packages/postgraphile-core/__tests__/integration/schema/omit-rename.omitstuff.test.js
@@ -1,7 +1,22 @@
 const core = require("./core");
 
-test("omitting some stuff", core.test(["d"], {}, `
+test("omit create, update and delete", core.test(["d"], {}, `
 comment on table d.films is E'@omit create,update,delete';
+`));
+
+test("omit create", core.test(["d"], {}, `
+comment on table d.films is E'@omit create';
+`));
+
+test("omit update", core.test(["d"], {}, `
+comment on table d.films is E'@omit update';
+`));
+
+test("omit delete", core.test(["d"], {}, `
+comment on table d.films is E'@omit delete';
+`));
+
+test("omit and omit many", core.test(["d"], {}, `
 comment on table d.tv_shows is E'@omit';
 comment on table d.tv_episodes is E'@omit many';
 `));
