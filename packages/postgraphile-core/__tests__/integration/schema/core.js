c0dbc84f458473bae93c078608ce57b961914091
Jem <jem@jemjie.com>
2018-04-04 12:18:19 +0100

    Refactored rename and omit tests into new files

--- packages/postgraphile-core/__tests__/integration/schema/core.js
+++ packages/postgraphile-core/__tests__/integration/schema/core.js
@@ -2,8 +2,15 @@ const printSchemaOrdered = require("../../printSchemaOrdered");
 const { withPgClient } = require("../../helpers");
 const { createPostGraphileSchema } = require("../../..");
 
-exports.test = (schemas, options) => () =>
+exports.test = (schemas, options, setup) => () =>
   withPgClient(async client => {
+    if (setup) {
+      if (typeof setup === 'function') {
+        await setup(client);
+      } else {
+        await client.query(setup);
+      }
+    }
     const schema = await createPostGraphileSchema(client, schemas, options);
     expect(printSchemaOrdered(schema)).toMatchSnapshot();
   });
