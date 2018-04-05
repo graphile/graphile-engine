23ee8caf31e71a8218cf3bb8bed841731e809b82
Jem <jem@jemjie.com>
2018-04-05 14:14:01 +0100

    tests omit read on a column

--- packages/postgraphile-core/__tests__/integration/schema/omit-rename.omitcolumns.test.js
+++ packages/postgraphile-core/__tests__/integration/schema/omit-rename.omitcolumns.test.js
@@ -8,6 +8,10 @@ test("omit update on column", core.test(["d"], {}, `
 comment on column d.tv_shows.title is E'@omit update';
 `));
 
+test("omit read on column", core.test(["d"], {}, `
+comment on column d.tv_shows.title is E'@omit read,create,update,delete,all,many';
+`));
+
 test("omit on column", core.test(["d"], {}, `
 comment on column d.tv_shows.title is E'@omit *';
 comment on column d.tv_episodes.title is E'@omit';
