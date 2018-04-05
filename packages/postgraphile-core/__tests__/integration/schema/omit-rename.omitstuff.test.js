e2a40de3f958e5ac20d558d8a3410367dfca7e9a
Jem <jem@jemjie.com>
2018-04-05 13:54:27 +0100

    test for omit create on column

--- packages/postgraphile-core/__tests__/integration/schema/omit-rename.omitstuff.test.js
+++ packages/postgraphile-core/__tests__/integration/schema/omit-rename.omitstuff.test.js
@@ -1,28 +1,32 @@
 const core = require("./core");
 
-test("omit create, update and delete", core.test(["d"], {}, `
+test("omit create, update and delete on table", core.test(["d"], {}, `
 comment on table d.films is E'@omit create,update,delete';
 `));
 
-test("omit create", core.test(["d"], {}, `
+test("omit create on table", core.test(["d"], {}, `
 comment on table d.films is E'@omit create';
 `));
 
-test("omit update", core.test(["d"], {}, `
+test("omit update on table", core.test(["d"], {}, `
 comment on table d.films is E'@omit update';
 `));
 
-test("omit delete", core.test(["d"], {}, `
+test("omit delete on table", core.test(["d"], {}, `
 comment on table d.films is E'@omit delete';
 `));
 
-test("omit read", core.test(["d"], {}, `
+test("omit read on table", core.test(["d"], {}, `
 comment on table d.films is E'@omit read,all,update,create,delete,many';
 `));
 
 
-test("omit and omit many", core.test(["d"], {}, `
+test("omit and omit many on table", core.test(["d"], {}, `
 comment on table d.films is E'@omit *';
 comment on table d.tv_shows is E'@omit';
 comment on table d.tv_episodes is E'@omit many';
+`));
+
+test("omit on column", core.test(["d"], {}, `
+comment on column d.tv_shows.title is E'@omit create';
 `));
