c5f81701afce481b11cc4b1e78d738530e5a6bff
Jem <jem@jemjie.com>
2018-04-05 13:55:50 +0100

    test for omit create on column

---
+++ packages/postgraphile-core/__tests__/integration/schema/omit-rename.omitcolumns.test.js
@@ -0,0 +1,8 @@
+const core = require("./core");
+
+test("omit create on column", core.test(["d"], {}, `
+comment on column d.tv_shows.title is E'@omit create';
+`));
+
+test("omit update on column", core.test(["d"], {}, `
+`));
