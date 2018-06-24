346c8f7dc7f74046c421e88abbc0bd1db48b23ce
Benjie Gillam <benjie@jemjie.com>
2018-06-24 12:49:47 +0100

    Test with and without RBAC

--- packages/postgraphile-core/__tests__/integration/schema/rbac.test.js
+++ packages/postgraphile-core/__tests__/integration/schema/rbac.test.js
@@ -2,8 +2,14 @@ const core = require("./core");
 const fs = require("fs");
 
 test(
-  "prints a schema with the default options using RBAC permissions",
+  "prints a schema from non-root role, using RBAC permissions",
   core.test(["a", "b", "c"], {}, client =>
+    client.query("set role postgraphile_test_visitor")
+  )
+);
+test(
+  "prints a schema from non-root role, with RBAC ignored",
+  core.test(["a", "b", "c"], { ignoreRBAC: true }, client =>
     client.query("set role postgraphile_test_visitor")
   )
 );
