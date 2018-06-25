5183f2c204ea80e6ffe395cffdbbea5b5bb96631
Benjie Gillam <benjie@jemjie.com>
2018-06-25 12:40:24 +0100

    More helpful error swallowing

--- packages/graphile-build/src/swallowError.js
+++ packages/graphile-build/src/swallowError.js
@@ -5,11 +5,13 @@ const debugWarn = debugFactory("graphile-build:warn");
 
 export default function swallowError(e: Error): void {
   // XXX: Improve this
-  if (!debugWarn.enabled) {
-    // eslint-disable-next-line no-console
-    console.warn(
-      `An error occurred, it might be okay but it doesn't look like the error we were expecting... run with envvar 'DEBUG="graphile-build:warn"' to view the error`
-    );
-  }
+  // eslint-disable-next-line no-console
+  console.warn(
+    `An error occurred, it might be okay but it doesn't look like the error we were expecting... ${
+      debugWarn.enabled
+        ? ""
+        : `run with envvar 'DEBUG="graphile-build:warn"' to view the error`
+    }`
+  );
   debugWarn(e);
 }
