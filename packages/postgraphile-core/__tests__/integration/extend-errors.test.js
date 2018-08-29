5b4aa38a0d9e1f9a30b2c502291d919df8c9eb8f
Benjie Gillam <benjie@jemjie.com>
2018-08-29 09:51:57 +0100

    Test for existing errors

---
+++ packages/postgraphile-core/__tests__/integration/extend-errors.test.js
@@ -0,0 +1,27 @@
+const { withPgClient } = require("../helpers");
+const { createPostGraphileSchema } = require("../..");
+
+function check(description, sql) {
+  test(description, async () => {
+    let error;
+    await withPgClient(async pgClient => {
+      await pgClient.query(sql);
+      try {
+        await createPostGraphileSchema(pgClient, ["a", "b", "c"], {
+          simpleCollections: "both",
+        });
+      } catch (e) {
+        error = e;
+      }
+    });
+    expect(error).toBeTruthy();
+    expect(error).toMatchSnapshot();
+  });
+}
+
+check(
+  "simple collections naming clash",
+  `
+    comment on constraint post_author_id_fkey on a.post is E'@foreignFieldName clash';
+  `
+);
