3e85f0d308deb17678ce73b1e40797ae57a7b13f
Benjie Gillam <benjie@jemjie.com>
2018-08-29 10:00:54 +0100

    Storing hints from both sides

--- packages/graphile-build/src/extend.js
+++ packages/graphile-build/src/extend.js
@@ -9,14 +9,25 @@ export default function extend<Obj1: *, Obj2: *>(
 ): Obj1 & Obj2 {
   const keysA = Object.keys(base);
   const keysB = Object.keys(extra);
+  const hints = Object.create(null);
   for (const key of keysB) {
     const newValue = extra[key];
     const oldValue = base[key];
     if (aExtendedB.get(newValue) !== oldValue && keysA.indexOf(key) >= 0) {
       throw new Error(`Overwriting key '${key}' is not allowed! ${hint || ""}`);
     }
+    const hintKey = `_source__${key}`;
+    hints[hintKey] = extra[hintKey] || base[hintKey] || hint;
   }
   const obj = Object.assign({}, base, extra);
   aExtendedB.set(obj, base);
+  for (const hintKey in hints) {
+    Object.defineProperty(obj, hintKey, {
+      configurable: false,
+      enumerable: false,
+      value: hints[hintKey],
+      writable: false,
+    });
+  }
   return obj;
 }
