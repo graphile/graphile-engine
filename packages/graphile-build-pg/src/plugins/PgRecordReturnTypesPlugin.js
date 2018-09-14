9eab3494561266ac80ba01d56e823fb92f765c2b
Matt Bretl <mpbretl@gmail.com>
2018-09-14 14:53:19 +0100

    Add pgIntrospection: proc to record return type scope

--- packages/graphile-build-pg/src/plugins/PgRecordReturnTypesPlugin.js
+++ packages/graphile-build-pg/src/plugins/PgRecordReturnTypesPlugin.js
@@ -86,6 +86,7 @@ export default (function PgRecordReturnTypesPlugin(builder) {
               }
             )}`,
             isRecordReturnType: true,
+            pgIntrospection: proc,
           }
         );
       });
