a7f3a37510cd65fed5e28d03a820238ab0e2bbd3
Benjie Gillam <benjie@jemjie.com>
2019-02-04 21:11:18 +0000

    Basically functioning socket server

--- packages/lds/src/index.ts
+++ packages/lds/src/index.ts
@@ -1,12 +1,21 @@
-/* tslint:disable no-console */
+/* tslint:disable no-console curly */
 import PgLogicalDecoding, {
   changeToRecord,
   changeToPk,
 } from "./pg-logical-decoding";
 
+import * as WebSocket from "ws";
+
+const stringify = JSON.stringify;
+
 const CONNECTION_STRING = process.env.LD_DATABASE_URL;
 const TABLE_PATTERN = process.env.LD_TABLE_PATTERN || "*.*";
 const SLOT_NAME = process.env.LD_SLOT_NAME || "postgraphile";
+const PORT =
+  parseInt(process.env.LD_PORT || process.env.PORT || "", 10) || 9876;
+
+// Maximum number of expected clients
+const SLOTS = parseInt(process.env.LD_SLOTS || "", 10) || 50;
 
 const SLEEP_DURATION = Math.max(
   1,
@@ -36,8 +45,108 @@ async function main() {
       console.trace(e);
     }
   }
-  function announce(topicParts: Array<string>, payload: {} | null = null) {
-    console.log(topicParts.join("|"), payload);
+
+  // Now slot is created, create websocket server
+  const wss = new WebSocket.Server({ port: PORT });
+  const channels: {
+    [schema: string]: {
+      [table: string]: {
+        [stringifiedKey: string]: Array<WebSocket | null>;
+      };
+    };
+  } = {};
+
+  wss.on("connection", function connection(ws) {
+    ws.on("message", function incoming(rawMessage) {
+      const message = rawMessage.toString("utf8");
+      let topicJSON: string;
+      let sub: boolean;
+      if (message.startsWith("SUB ")) {
+        topicJSON = message.substr(4);
+        sub = true;
+      } else if (message.startsWith("UNSUB ")) {
+        topicJSON = message.substr(6);
+        sub = false;
+      } else {
+        console.error("Unknown command", message);
+        return;
+      }
+      const topic = JSON.parse(topicJSON);
+      if (!Array.isArray(topic)) return console.error("Not an array");
+      if (topic.length < 2) return console.error("Too short");
+      if (topic.length > 3) return console.error("Too long");
+      const [schema, table, key] = topic;
+      if (typeof schema !== "string")
+        return console.error("Schema not a string");
+      if (typeof table !== "string") return console.error("Table not a string");
+      if (
+        key &&
+        (!Array.isArray(key) || key.some((s: unknown) => typeof s !== "string"))
+      )
+        return console.error("Invalid key spec");
+
+      const stringifiedKey = stringify(key);
+      if (!channels[schema]) {
+        channels[schema] = {};
+      }
+      if (!channels[schema][table]) {
+        channels[schema][table] = {};
+      }
+      if (!channels[schema][table][stringifiedKey]) {
+        channels[schema][table][stringifiedKey] = new Array(SLOTS);
+      }
+      const sockets = channels[schema][table][stringifiedKey]!;
+      const i = sockets.indexOf(ws);
+      if (sub) {
+        if (i >= 0) {
+          console.error("Socket is already registered for ", stringifiedKey);
+          return;
+        }
+        const emptyIndex = sockets.findIndex(s => !s);
+        if (emptyIndex < 0) {
+          console.error("All sockets are full");
+          return;
+        }
+        sockets[emptyIndex] = ws;
+      } else {
+        if (i < 0) {
+          console.error("Socket is not registered for ", stringifiedKey);
+          return;
+        }
+        sockets[i] = null;
+      }
+    });
+
+    ws.send("ACK");
+  });
+
+  function announce(
+    [schema, table, key]: [string, string] | [string, string, Array<string>],
+    {
+      kind,
+      data = null,
+    }: { kind: "insert" | "update" | "delete"; data?: {} | null }
+  ) {
+    if (!channels[schema] || !channels[schema][table]) return;
+    const stringifiedKey = stringify(key);
+    const sockets = channels[schema][table][stringifiedKey];
+    if (!sockets) return;
+    for (const socket of sockets) {
+      if (socket) {
+        socket.send(
+          JSON.stringify({
+            kind,
+            schema,
+            table,
+            key,
+            data,
+          })
+        );
+      }
+    }
+    if (kind === "delete") {
+      delete channels[schema][table][stringifiedKey];
+    }
   }
   while (true) {
     const rows = await client.getChanges(null, 500);
@@ -56,12 +165,12 @@ async function main() {
               data: changeToRecord(change),
             });
           } else if (change.kind === "update") {
-            announce([schema, table, ...changeToPk(change)], {
+            announce([schema, table, changeToPk(change)], {
               kind,
               data: changeToRecord(change),
             });
           } else if (change.kind === "delete") {
-            announce([schema, table, ...changeToPk(change)], {
+            announce([schema, table, changeToPk(change)], {
               kind,
             });
           } else {
