766285b3bc5b7be2f2f2a6ace6a6bfd9c6dc229b
Benjie Gillam <benjie@jemjie.com>
2019-05-07 11:47:52 +0100

    Precompute for performance

--- packages/graphile-build-pg/src/plugins/PgRowByUniqueConstraint.js
+++ packages/graphile-build-pg/src/plugins/PgRowByUniqueConstraint.js
@@ -68,28 +68,58 @@ export default (async function PgRowByUniqueConstraint(
                 table,
                 constraint
               );
+              const keysIncludingMeta = keys.map(key => ({
+                ...key,
+                sqlIdentifier: sql.identifier(key.name),
+                columnName: inflection.column(key),
+              }));
+              // Precomputation for performance
+              const queryFromResolveDataOptions = {
+                useAsterisk: false, // Because it's only a single relation, no need
+              };
+              const queryFromResolveDataCallback = (queryBuilder, args) => {
+                if (subscriptions && table.primaryKeyConstraint) {
+                  queryBuilder.selectIdentifiers(table);
+                }
+                const sqlTableAlias = queryBuilder.getTableAlias();
+                keysIncludingMeta.forEach(
+                  ({ sqlIdentifier, columnName, type, typeModifier }) => {
+                    queryBuilder.where(
+                      sql.fragment`${sqlTableAlias}.${sqlIdentifier} = ${gql2pg(
+                        args[columnName],
+                        type,
+                        typeModifier
+                      )}`
+                    );
+                  }
+                );
+              };
+
               memo[fieldName] = fieldWithHooks(
                 fieldName,
                 ({ getDataFromParsedResolveInfoFragment }) => {
                   return {
                     type: TableType,
-                    args: keys.reduce((memo, key) => {
-                      const InputType = pgGetGqlInputTypeByTypeIdAndModifier(
-                        key.typeId,
-                        key.typeModifier
-                      );
-                      if (!InputType) {
-                        throw new Error(
-                          `Could not find input type for key '${
-                            key.name
-                          }' on type '${TableType.name}'`
+                    args: keysIncludingMeta.reduce(
+                      (memo, { typeId, typeModifier, columnName, name }) => {
+                        const InputType = pgGetGqlInputTypeByTypeIdAndModifier(
+                          typeId,
+                          typeModifier
                         );
-                      }
-                      memo[inflection.column(key)] = {
-                        type: new GraphQLNonNull(InputType),
-                      };
-                      return memo;
-                    }, {}),
+                        if (!InputType) {
+                          throw new Error(
+                            `Could not find input type for key '${name}' on type '${
+                              TableType.name
+                            }'`
+                          );
+                        }
+                        memo[columnName] = {
+                          type: new GraphQLNonNull(InputType),
+                        };
+                        return memo;
+                      },
+                      {}
+                    ),
                     async resolve(parent, args, resolveContext, resolveInfo) {
                       const { pgClient } = resolveContext;
                       const liveRecord =
@@ -107,25 +137,9 @@ export default (async function PgRowByUniqueConstraint(
                         sqlFullTableName,
                         undefined,
                         resolveData,
-                        {
-                          useAsterisk: false, // Because it's only a single relation, no need
-                        },
-                        queryBuilder => {
-                          if (subscriptions && table.primaryKeyConstraint) {
-                            queryBuilder.selectIdentifiers(table);
-                          }
-                          keys.forEach(key => {
-                            queryBuilder.where(
-                              sql.fragment`${queryBuilder.getTableAlias()}.${sql.identifier(
-                                key.name
-                              )} = ${gql2pg(
-                                args[inflection.column(key)],
-                                key.type,
-                                key.typeModifier
-                              )}`
-                            );
-                          });
-                        },
+                        queryFromResolveDataOptions,
+                        queryBuilder =>
+                          queryFromResolveDataCallback(queryBuilder, args),
                         resolveContext,
                         resolveInfo.rootValue
                       );
