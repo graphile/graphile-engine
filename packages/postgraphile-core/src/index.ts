f23fa3219cabfd00a6cec737caf6082bb070e7a8
tinymarsracing <37403227+tinymarsracing@users.noreply.github.com>
2019-05-17 15:31:20 +0200

    Update index.ts

--- packages/postgraphile-core/src/index.ts
+++ packages/postgraphile-core/src/index.ts
@@ -101,6 +101,7 @@ export interface PostGraphileCoreOptions {
   ignoreRBAC?: boolean;
   legacyFunctionsOnly?: boolean;
   ignoreIndexes?: boolean;
+  hideIndexWarnings?: boolean;
   subscriptions?: boolean;
   live?: boolean;
   ownerConnectionString?: string;
@@ -219,6 +220,7 @@ const getPostGraphileBuilder = async (
     ignoreRBAC = true, // TODO:v5: Change to 'false' in v5
     legacyFunctionsOnly = false, // TODO:v5: Remove in v5
     ignoreIndexes = true, // TODO:v5: Change to 'false' in v5
+    hideIndexWarnings = false,
     subscriptions: inSubscriptions = false, // TODO:v5: Change to 'true' in v5
     live = false,
     ownerConnectionString,
@@ -380,6 +382,7 @@ const getPostGraphileBuilder = async (
     pgIgnoreRBAC: ignoreRBAC,
     pgLegacyFunctionsOnly: legacyFunctionsOnly,
     pgIgnoreIndexes: ignoreIndexes,
+    pgHideIndexWarnings: hideIndexWarnings,
     pgOwnerConnectionString: ownerConnectionString,
 
     /*
