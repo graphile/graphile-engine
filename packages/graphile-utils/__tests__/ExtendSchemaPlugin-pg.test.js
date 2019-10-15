400c2d1d81862c6862227fcf166011eba20b8452
Lenz Weber <lenz.weber@mayflower.de>
2019-10-15 16:07:24 +0200

    test(postgraphile) feedback from review & regenerate snapshots

--- packages/graphile-utils/__tests__/ExtendSchemaPlugin-pg.test.js
+++ packages/graphile-utils/__tests__/ExtendSchemaPlugin-pg.test.js
@@ -722,8 +722,11 @@ it("allows to retrieve a single scalar value", async () => {
               myCustomScalar: Int! @pgQuery(
                 fragment: ${embed(sql.fragment`(SELECT 100)`)}
               )
-              myCustomScalarWithFunction: Int! @pgQuery(
-                fragment: ${embed(queryBuilder => sql.fragment`(SELECT 101)`)}
+              myCustomScalarWithFunction: String! @pgQuery(
+                fragment: ${embed(
+                  queryBuilder =>
+                    sql.fragment`(${queryBuilder.getTableAlias()}.name || ' ' || ${queryBuilder.getTableAlias()}.email)`
+                )}
               )
               myCustomScalarWithFunctionAndArgument(test: Int!): Int! @pgQuery(
                 fragment: ${embed(
@@ -737,8 +740,7 @@ it("allows to retrieve a single scalar value", async () => {
       }),
     ],
   });
-  const printedSchema = printSchema(schema);
-  expect(printedSchema).toMatchSnapshot();
+  expect(schema).toMatchSnapshot();
   const pgClient = await pgPool.connect();
   try {
     const { data, errors } = await graphql(
@@ -751,6 +753,9 @@ it("allows to retrieve a single scalar value", async () => {
             myCustomScalar
             myCustomScalarWithFunction
             myCustomScalarWithFunctionAndArgument(test: 102)
+            m100: myCustomScalar
+            mAlice: myCustomScalarWithFunction
+            m103: myCustomScalarWithFunctionAndArgument(test: 103)
           }
         }
       `,
@@ -762,8 +767,13 @@ it("allows to retrieve a single scalar value", async () => {
     expect(data).toBeTruthy();
     expect(data.user).toBeTruthy();
     expect(data.user.myCustomScalar).toBe(100);
-    expect(data.user.myCustomScalarWithFunction).toBe(101);
+    expect(data.user.myCustomScalarWithFunction).toBe(
+      "Alice alice@example.com"
+    );
     expect(data.user.myCustomScalarWithFunctionAndArgument).toBe(102);
+    expect(data.user.m100).toBe(100);
+    expect(data.user.mAlice).toBe("Alice alice@example.com");
+    expect(data.user.m103).toBe(103);
   } finally {
     pgClient.release();
   }
@@ -780,7 +790,7 @@ it("allows to retrieve array scalar values", async () => {
             extend type User {
               myCustomArrayOfScalars: [String!]! @pgQuery(
                 fragment: ${embed(
-                  sql.fragment`array(SELECT name from ${sql.fragment`graphile_utils.pets`})`
+                  sql.fragment`array(SELECT name from graphile_utils.pets)`
                 )}
               )
               
@@ -790,8 +800,7 @@ it("allows to retrieve array scalar values", async () => {
       }),
     ],
   });
-  const printedSchema = printSchema(schema);
-  expect(printedSchema).toMatchSnapshot();
+  expect(schema).toMatchSnapshot();
   const pgClient = await pgPool.connect();
   try {
     const { data, errors } = await graphql(
