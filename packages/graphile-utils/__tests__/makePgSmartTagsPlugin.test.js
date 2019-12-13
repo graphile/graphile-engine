db490cfb5258d7350f7a7d9821d27331062ceed1
Benjie Gillam <benjie@jemjie.com>
2019-12-13 09:16:23 +0000

    Refactor to test.each

--- packages/graphile-utils/__tests__/makePgSmartTagsPlugin.test.js
+++ packages/graphile-utils/__tests__/makePgSmartTagsPlugin.test.js
@@ -41,12 +41,143 @@ async function withContext(callback) {
   }
 }
 
-async function testJSON(json) {
+/*
+ * The JSON format is quite flexible, there's often a few different ways of
+ * expressing the same thing; these non-exhaustive tests check that the
+ * different variants all work as expected.
+ */
+test.each([
+  [
+    "fully qualified table name, embedded column",
+    {
+      version: 1,
+      config: {
+        class: {
+          // Fully qualified table name
+          "graphile_utils.test_smart_tags": {
+            tags: {
+              omit: "",
+              primaryKey: "email",
+              foreignKey: "(email) references graphile_utils.users (email)",
+            },
+            attribute: {
+              // Column name embedded inside table
+              value: {
+                tags: {
+                  name: "val",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  ],
+  [
+    "simple table name, embedded column",
+    {
+      version: 1,
+      config: {
+        class: {
+          // Just table name (no schema)
+          test_smart_tags: {
+            tags: {
+              omit: "",
+              primaryKey: "email",
+              foreignKey: "(email) references graphile_utils.users (email)",
+            },
+            attribute: {
+              value: {
+                tags: {
+                  name: "val",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  ],
+  [
+    "simple table name, table.column",
+    {
+      version: 1,
+      config: {
+        class: {
+          test_smart_tags: {
+            tags: {
+              omit: "",
+              primaryKey: "email",
+              foreignKey: "(email) references graphile_utils.users (email)",
+            },
+          },
+        },
+        attribute: {
+          // Just table name and column name
+          "test_smart_tags.value": {
+            tags: {
+              name: "val",
+            },
+          },
+        },
+      },
+    },
+  ],
+  [
+    "simple table name, fully qualified column",
+    {
+      version: 1,
+      config: {
+        class: {
+          test_smart_tags: {
+            tags: {
+              omit: "",
+              primaryKey: "email",
+              foreignKey: "(email) references graphile_utils.users (email)",
+            },
+          },
+        },
+        attribute: {
+          // Fully qualified column name
+          "graphile_utils.test_smart_tags.value": {
+            tags: {
+              name: "val",
+            },
+          },
+        },
+      },
+    },
+  ],
+  [
+    "simple table name, simple column name",
+    {
+      version: 1,
+      config: {
+        class: {
+          test_smart_tags: {
+            tags: {
+              omit: "",
+              primaryKey: "email",
+              foreignKey: "(email) references graphile_utils.users (email)",
+            },
+          },
+        },
+        attribute: {
+          // This matches _all_ columns called 'value' (in all tables)
+          value: {
+            tags: {
+              name: "val",
+            },
+          },
+        },
+      },
+    },
+  ],
+])("equivalent JSON config (%s)", async (title, json) => {
   const schema = await createPostGraphileSchema(pgPool, ["graphile_utils"], {
     disableDefaultMutations: true,
     appendPlugins: [makeJSONPgSmartTagsPlugin(json)],
   });
-  expect(schema).toMatchSnapshot();
   await withContext(async context => {
     const { data, errors } = await graphql(
       schema,
@@ -82,130 +213,4 @@ async function testJSON(json) {
       },
     });
   });
-}
-
-/*
- * The JSON format is quite flexible, there's often a few different ways of
- * expressing the same thing; these non-exhaustive tests check that the
- * different variants all work as expected.
- */
-it("Equivalent JSON test 1", () =>
-  testJSON({
-    version: 1,
-    config: {
-      class: {
-        // Fully qualified table name
-        "graphile_utils.test_smart_tags": {
-          tags: {
-            omit: "",
-            primaryKey: "email",
-            foreignKey: "(email) references graphile_utils.users (email)",
-          },
-          attribute: {
-            // Column name embedded inside table
-            value: {
-              tags: {
-                name: "val",
-              },
-            },
-          },
-        },
-      },
-    },
-  }));
-
-it("Equivalent JSON test 2", () =>
-  testJSON({
-    version: 1,
-    config: {
-      class: {
-        // Just table name (no schema)
-        test_smart_tags: {
-          tags: {
-            omit: "",
-            primaryKey: "email",
-            foreignKey: "(email) references graphile_utils.users (email)",
-          },
-          attribute: {
-            value: {
-              tags: {
-                name: "val",
-              },
-            },
-          },
-        },
-      },
-    },
-  }));
-
-it("Equivalent JSON test 3", () =>
-  testJSON({
-    version: 1,
-    config: {
-      class: {
-        test_smart_tags: {
-          tags: {
-            omit: "",
-            primaryKey: "email",
-            foreignKey: "(email) references graphile_utils.users (email)",
-          },
-        },
-      },
-      attribute: {
-        // Just table name and column name
-        "test_smart_tags.value": {
-          tags: {
-            name: "val",
-          },
-        },
-      },
-    },
-  }));
-
-it("Equivalent JSON test 4", () =>
-  testJSON({
-    version: 1,
-    config: {
-      class: {
-        test_smart_tags: {
-          tags: {
-            omit: "",
-            primaryKey: "email",
-            foreignKey: "(email) references graphile_utils.users (email)",
-          },
-        },
-      },
-      attribute: {
-        // Fully qualified column name
-        "graphile_utils.test_smart_tags.value": {
-          tags: {
-            name: "val",
-          },
-        },
-      },
-    },
-  }));
-
-it("Equivalent JSON test 5", () =>
-  testJSON({
-    version: 1,
-    config: {
-      class: {
-        test_smart_tags: {
-          tags: {
-            omit: "",
-            primaryKey: "email",
-            foreignKey: "(email) references graphile_utils.users (email)",
-          },
-        },
-      },
-      attribute: {
-        // This matches _all_ columns called 'value' (in all tables)
-        value: {
-          tags: {
-            name: "val",
-          },
-        },
-      },
-    },
-  }));
+});
