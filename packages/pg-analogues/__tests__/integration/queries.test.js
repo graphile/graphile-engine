69fd6df2666d92690885aa0afbfadc6653c41012
Benjie Gillam <benjie@jemjie.com>
2021-07-02 17:22:21 +0100

    First query test

---
+++ packages/pg-analogues/__tests__/integration/queries.test.js
@@ -0,0 +1,291 @@
+const { graphql } = require("graphql");
+const { withPgClient, getServerVersionNum } = require("../helpers");
+const { createPostGraphileSchema } = require("postgraphile-core");
+const { readdirSync, readFile: rawReadFile } = require("fs");
+const { resolve: resolvePath } = require("path");
+const { printSchema } = require("graphql/utilities");
+const debug = require("debug")("graphile-build:schema");
+const { makeExtendSchemaPlugin, gql } = require("graphile-utils");
+const ToyCategoriesPlugin = require("./ToyCategoriesPlugin");
+
+function readFile(filename, encoding) {
+  return new Promise((resolve, reject) => {
+    rawReadFile(filename, encoding, (err, res) => {
+      if (err) reject(err);
+      else resolve(res);
+    });
+  });
+}
+
+const queriesDir = `${__dirname}/../fixtures/queries`;
+const queryFileNames = readdirSync(queriesDir);
+const testsToSkip = [];
+let queryResults = [];
+
+const kitchenSinkData = () =>
+  readFile(`${__dirname}/../kitchen-sink-data.analogue.sql`, "utf8");
+
+const pg11Data = () =>
+  readFile(`${__dirname}/../pg11-data.analogue.sql`, "utf8");
+
+const dSchemaComments = () =>
+  readFile(
+    `${__dirname}/../kitchen-sink-d-schema-comments.analogue.sql`,
+    "utf8"
+  );
+
+beforeAll(() => {
+  // Get a few GraphQL schema instance that we can query.
+  console.log("beforeAll withPgClient");
+  const gqlSchemasPromise = withPgClient(async pgClient => {
+    console.log("beforeAll getVersion");
+    const serverVersionNum = await getServerVersionNum(pgClient);
+    if (serverVersionNum < 90500) {
+      // Remove tests not supported by PG9.4
+      testsToSkip.push("json-overflow.graphql");
+    }
+
+    console.log("beforeAll add dSchemaComments");
+    // A selection of omit/rename comments on the d schema
+    await pgClient.query(await dSchemaComments());
+    console.log("beforeAll building schemas");
+
+    // Different fixtures need different schemas with different configurations.
+    // Make all of the different schemas with different configurations that we
+    // need and wait for them to be created in parallel.
+    const [
+      normal,
+      classicIds,
+      dynamicJson,
+      pgColumnFilter,
+      viewUniqueKey,
+      dSchema,
+      simpleCollections,
+      orderByNullsLast,
+      smartCommentRelations,
+      largeBigint,
+      useCustomNetworkScalars,
+      pg11UseCustomNetworkScalars,
+      namedQueryBuilder,
+      enumTables,
+      geometry,
+    ] = await Promise.all([
+      createPostGraphileSchema(pgClient, ["a", "b", "c"], {
+        subscriptions: true,
+        appendPlugins: [
+          makeExtendSchemaPlugin({
+            typeDefs: gql`
+              extend type Query {
+                extended: Boolean
+              }
+            `,
+            resolvers: {
+              Query: {
+                extended: () => true,
+              },
+            },
+          }),
+        ],
+      }),
+      /*
+      createPostGraphileSchema(pgClient, ["a", "b", "c"], {
+        subscriptions: true,
+        classicIds: true,
+      }),
+      createPostGraphileSchema(pgClient, ["a", "b", "c"], {
+        subscriptions: true,
+        dynamicJson: true,
+        setofFunctionsContainNulls: null,
+      }),
+      createPostGraphileSchema(pgClient, ["a", "b", "c"], {
+        subscriptions: true,
+        pgColumnFilter: attr => attr.name !== "headline",
+        setofFunctionsContainNulls: false,
+      }),
+      createPostGraphileSchema(pgClient, ["a", "b", "c"], {
+        subscriptions: true,
+        viewUniqueKey: "testviewid",
+        setofFunctionsContainNulls: true,
+      }),
+      createPostGraphileSchema(pgClient, ["d"], {}),
+      createPostGraphileSchema(pgClient, ["a", "b", "c"], {
+        subscriptions: true,
+        simpleCollections: "both",
+      }),
+      createPostGraphileSchema(pgClient, ["a"], {
+        subscriptions: true,
+        graphileBuildOptions: {
+          orderByNullsLast: true,
+        },
+      }),
+      createPostGraphileSchema(pgClient, ["smart_comment_relations"], {}),
+      createPostGraphileSchema(pgClient, ["large_bigint"], {}),
+      createPostGraphileSchema(pgClient, ["network_types"], {
+        graphileBuildOptions: {
+          pgUseCustomNetworkScalars: true,
+        },
+      }),
+         null,
+      createPostGraphileSchema(pgClient, ["named_query_builder"], {
+        subscriptions: true,
+        appendPlugins: [ToyCategoriesPlugin],
+      }),
+      createPostGraphileSchema(pgClient, ["enum_tables"], {
+        subscriptions: true,
+      }),
+      createPostGraphileSchema(pgClient, ["geometry"], {
+        subscriptions: true,
+        graphileBuildOptions: {
+          pgGeometricTypes: true,
+        },
+      }),
+      */
+    ]);
+    console.log("beforeAll schemas built");
+    debug(printSchema(normal));
+    return {
+      normal,
+      classicIds,
+      dynamicJson,
+      pgColumnFilter,
+      viewUniqueKey,
+      dSchema,
+      simpleCollections,
+      orderByNullsLast,
+      smartCommentRelations,
+      largeBigint,
+      useCustomNetworkScalars,
+      pg11UseCustomNetworkScalars,
+      namedQueryBuilder,
+      enumTables,
+      geometry,
+    };
+  });
+
+  // Execute all of the queries in parallel. We will not wait for them to
+  // resolve or reject. The tests will do that.
+  //
+  // All of our queries share a single client instance.
+  const queryResultsPromise = (async () => {
+    // Wait for the schema to resolve. We need the schema to be introspected
+    // before we can do anything else!
+    const gqlSchemas = await gqlSchemasPromise;
+    console.log("withPgClient");
+    // Get a new Postgres client instance.
+    return await withPgClient(async pgClient => {
+      // Add data to the client instance we are using.
+      console.log("Loading data");
+      await pgClient.query(await kitchenSinkData());
+      console.log("Getting version");
+      const serverVersionNum = await getServerVersionNum(pgClient);
+      // Run all of our queries in parallel.
+      const results = [];
+      for (const filename of queryFileNames) {
+        console.log(`Processing '${filename}'`);
+        if (testsToSkip.indexOf(filename) >= 0) {
+          results.push(Promise.resolve());
+          continue;
+        }
+        const process = async fileName => {
+          console.log("Loading query");
+          // Read the query from the file system.
+          const query = await readFile(
+            resolvePath(queriesDir, fileName),
+            "utf8"
+          );
+          console.log(`Loaded query (${query.length})`);
+          // Get the appropriate GraphQL schema for this fixture. We want to test
+          // some specific fixtures against a schema configured slightly
+          // differently.
+          const schemas = {
+            "classic-ids.graphql": gqlSchemas.classicIds,
+            "dynamic-json.graphql": gqlSchemas.dynamicJson,
+            "dynamic-json.condition-json-field-variable.graphql":
+              gqlSchemas.dynamicJson,
+            "view.graphql": gqlSchemas.viewUniqueKey,
+            "badlyBehavedFunction.graphql": gqlSchemas.viewUniqueKey,
+            "simple-collections.graphql": gqlSchemas.simpleCollections,
+            "simple-relations-head-tail.graphql": gqlSchemas.simpleCollections,
+            "simple-relations-tail-head.graphql": gqlSchemas.simpleCollections,
+            "simple-procedure-computed-fields.graphql":
+              gqlSchemas.simpleCollections,
+            "simple-procedure-query.graphql": gqlSchemas.simpleCollections,
+            "types.graphql": gqlSchemas.simpleCollections,
+            "orderByNullsLast.graphql": gqlSchemas.orderByNullsLast,
+            "network_types.graphql": gqlSchemas.useCustomNetworkScalars,
+            "pg11.network_types.graphql":
+              gqlSchemas.pg11UseCustomNetworkScalars,
+            "pg11.types.graphql": gqlSchemas.pg11UseCustomNetworkScalars,
+          };
+          let gqlSchema = schemas[fileName];
+          if (!gqlSchema) {
+            if (fileName.startsWith("d.")) {
+              gqlSchema = gqlSchemas.dSchema;
+            } else if (fileName.startsWith("rbac.")) {
+              gqlSchema = gqlSchemas.rbac;
+            } else if (fileName.startsWith("smart_comment_relations.")) {
+              gqlSchema = gqlSchemas.smartCommentRelations;
+            } else if (fileName.startsWith("large_bigint")) {
+              gqlSchema = gqlSchemas.largeBigint;
+            } else if (fileName.startsWith("named_query_builder")) {
+              gqlSchema = gqlSchemas.namedQueryBuilder;
+            } else if (fileName.startsWith("enum_tables.")) {
+              gqlSchema = gqlSchemas.enumTables;
+            } else if (fileName.startsWith("geometry.")) {
+              gqlSchema = gqlSchemas.geometry;
+            } else {
+              gqlSchema = gqlSchemas.normal;
+            }
+          }
+          console.log("Got schema");
+
+          await pgClient.query("savepoint test");
+          if (gqlSchema === gqlSchemas.rbac) {
+            await pgClient.query(
+              "select set_config('role', 'postgraphile_test_visitor', true), set_config('jwt.claims.user_id', '3', true)"
+            );
+          }
+
+          try {
+            console.log("Calling GraphQL");
+            // Return the result of our GraphQL query.
+            const result = await graphql(gqlSchema, query, null, {
+              pgClient: pgClient,
+            });
+            console.log("GraphQL complete");
+            if (result.errors) {
+              // eslint-disable-next-line no-console
+              console.log(result.errors.map(e => e.originalError || e));
+            }
+            return result;
+          } finally {
+            console.log("Finally");
+            await pgClient.query("rollback to savepoint test");
+          }
+        };
+        results.push(await process(filename));
+      }
+      return results;
+    });
+  })();
+
+  // Flatten out the query results promise.
+  queryResults = queryFileNames.map(async (_, i) => {
+    return await (await queryResultsPromise)[i];
+  });
+});
+
+for (let i = 0; i < queryFileNames.length; i++) {
+  const filename = queryFileNames[i];
+  test(filename, async () => {
+    if (testsToSkip.indexOf(filename) >= 0) {
+      // eslint-disable-next-line no-console
+      console.log(`SKIPPED '${filename}'`);
+      // Technically this will never be ran because we handle it in scripts/test
+    } else {
+      console.log("Awaiting...");
+      expect(await queryResults[i]).toMatchSnapshot();
+      console.log("Complete");
+    }
+  });
+}
