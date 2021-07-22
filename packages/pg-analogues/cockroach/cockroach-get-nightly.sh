d510593fdab9c920af3a123188497e9529d433e7
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2021-07-22 09:41:32 +0100

    Get cockroach for Mac

--- packages/pg-analogues/cockroach/cockroach-get-nightly.sh
+++ packages/pg-analogues/cockroach/cockroach-get-nightly.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
-curl -LO https://edge-binaries.cockroachdb.com/cockroach/cockroach.linux-gnu-amd64.LATEST
-chmod +x cockroach.linux-gnu-amd64.LATEST
-./cockroach.linux-gnu-amd64.LATEST version
+# https://binaries.cockroachdb.com/cockroach-v21.1.6.darwin-10.9-amd64.tgz
+# curl -LO https://edge-binaries.cockroachdb.com/cockroach/cockroach.linux-gnu-amd64.LATEST
+curl -o cockroach.LATEST -L https://edge-binaries.cockroachdb.com/cockroach/cockroach.darwin-amd64.LATEST
+chmod +x cockroach.LATEST
+./cockroach.LATEST version
