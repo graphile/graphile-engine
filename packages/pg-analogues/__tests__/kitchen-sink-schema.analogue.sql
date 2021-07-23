3aeb2542bba4d4096cea0101f6053857b9b6afe4
Benjie Gillam <benjie@jemjie.com>
2021-07-23 09:30:58 +0100

    Split schema enum inserts from schema SDL

--- packages/pg-analogues/__tests__/kitchen-sink-schema.analogue.sql
+++ packages/pg-analogues/__tests__/kitchen-sink-schema.analogue.sql
@@ -1178,31 +1178,6 @@ comment on table enum_tables.lots_of_enums is E'@omit';
 
 -- Enum table needs values added as part of the migration, not as part of the
 -- data.
-insert into enum_tables.abcd (letter, description) values
-  ('A', 'The letter A'),
-  ('B', 'The letter B'),
-  ('C', 'The letter C'),
-  ('D', 'The letter D');
-insert into enum_tables.lots_of_enums (enum_1, description) values
-  ('a1', 'Desc A1'),
-  ('a2', 'Desc A2'),
-  ('a3', 'Desc A3'),
-  ('a4', 'Desc A4');
-insert into enum_tables.lots_of_enums (enum_2, description) values
-  ('b1', 'Desc B1'),
-  ('b2', 'Desc B2'),
-  ('b3', 'Desc B3'),
-  ('b4', 'Desc B4');
-insert into enum_tables.lots_of_enums (enum_3, description) values
-  ('c1', 'Desc C1'),
-  ('c2', 'Desc C2'),
-  ('c3', 'Desc C3'),
-  ('c4', 'Desc C4');
-insert into enum_tables.lots_of_enums (enum_4, description) values
-  ('d1', 'Desc D1'),
-  ('d2', 'Desc D2'),
-  ('d3', 'Desc D3'),
-  ('d4', 'Desc D4');
 
 create table enum_tables.referencing_table(
   id serial primary key,
