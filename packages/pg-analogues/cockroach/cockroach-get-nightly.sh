12a2a485412fb0d55075f5ed7a2b17ee9c584fb3
Benjie Gillam <benjie@jemjie.com>
2021-07-28 13:53:47 +0100

    Download right version for current architecture

--- packages/pg-analogues/cockroach/cockroach-get-nightly.sh
+++ packages/pg-analogues/cockroach/cockroach-get-nightly.sh
@@ -2,6 +2,14 @@
 set -e
 # https://binaries.cockroachdb.com/cockroach-v21.1.6.darwin-10.9-amd64.tgz
 # curl -LO https://edge-binaries.cockroachdb.com/cockroach/cockroach.linux-gnu-amd64.LATEST
-curl -o cockroach.LATEST -L https://edge-binaries.cockroachdb.com/cockroach/cockroach.darwin-amd64.LATEST
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  VARIANT="linux-gnu-amd64"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  VARIANT="darwin-amd64"
+else
+  echo "Didn't recognize OSTYPE '$OSTYPE'" >&2
+  exit 1
+fi
+curl -o cockroach.LATEST -L https://edge-binaries.cockroachdb.com/cockroach/cockroach.$VARIANT.LATEST
 chmod +x cockroach.LATEST
 ./cockroach.LATEST version
