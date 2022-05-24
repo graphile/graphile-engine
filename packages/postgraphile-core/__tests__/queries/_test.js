6bdb452d28ac73f3b0d0bb4b5a133d7e46666a30
Benjie Gillam <benjie@jemjie.com>
2022-05-24 17:10:30 +0100

    Allow passing configuration via tests

--- packages/postgraphile-core/__tests__/queries/_test.js
+++ packages/postgraphile-core/__tests__/queries/_test.js
@@ -17,6 +17,8 @@ function readFile(filename, encoding) {
   });
 }
 
+const isNotNullish = t => t != null;
+
 const queriesDir = `${__dirname}/../fixtures/queries`;
 const queryFileNames = readdirSync(queriesDir);
 const testsToSkip = [];
@@ -30,35 +32,41 @@ const pg11Data = () => readFile(`${__dirname}/../pg11-data.sql`, "utf8");
 const dSchemaComments = () =>
   readFile(`${__dirname}/../kitchen-sink-d-schema-comments.sql`, "utf8");
 
-let schema;
-
-beforeAll(async () => {
-  await withPgClient(async pgClient => {
-    // A selection of omit/rename comments on the d schema
-    await pgClient.query(await dSchemaComments());
-
-    schema = await createPostGraphileSchema(pgClient, ["a", "b", "c"], {
-      subscriptions: true,
-      appendPlugins: [
-        makeExtendSchemaPlugin({
-          typeDefs: gql`
-            extend type Query {
-              extended: Boolean
-            }
-          `,
-          resolvers: {
-            Query: {
-              extended: () => true,
-            },
-          },
-        }),
-      ],
-    });
-  });
+const ExtendedPlugin = makeExtendSchemaPlugin({
+  typeDefs: gql`
+    extend type Query {
+      extended: Boolean
+    }
+  `,
+  resolvers: {
+    Query: {
+      extended: () => true,
+    },
+  },
 });
 
 exports.assertSnapshotsMatch = assertSnapshotsMatch;
 exports.runTestQuery = async (source, config, options) => {
+  const schema = await withPgClient(async pgClient => {
+    // A selection of omit/rename comments on the d schema
+    await pgClient.query(await dSchemaComments());
+    return createPostGraphileSchema(
+      pgClient,
+      config.schema ?? ["a", "b", "c"],
+      {
+        subscriptions: config.subscriptions,
+        classicIds: config.classicIds,
+        dynamicJson: config.dynamicJson,
+        setofFunctionsContainNulls: config.setofFunctionsContainNulls,
+        simpleCollections: config.simpleCollections,
+        graphileBuildOptions: config.graphileBuildOptions,
+        appendPlugins: [
+          ExtendedPlugin,
+          config.ToyCategoriesPlugin ? ToyCategoriesPlugin : null,
+        ].filter(isNotNullish),
+      }
+    );
+  });
   const onConnect = async pgClient => {
     // Add data to the client instance we are using.
     await pgClient.query(await kitchenSinkData());
